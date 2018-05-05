af84e7dadc1367b32b41af88c460cbffa7313885
Cody Boppert <cboppert@gmail.com>
2018-05-05 11:01:29 +0100

    Finish useless modules for now

--- modules/useless.erl
+++ modules/useless.erl
@@ -1,7 +1,11 @@
 -module(useless).
--export([add/2, hello/0]).
+-export([add/2, hello/0, greet_and_add_two/1]).
 
 add(X, Y) -> X + Y.
 
 % comments start with a %
 hello() -> io:format("Hello, world!~n").
+
+greet_and_add_two(X) ->
+   hello(),
+   add(X,2).
